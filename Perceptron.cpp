61024afca748298bf0b5cf748e1605116c80f202
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2016-05-14 16:09:56 -0300

    Merge origin/master
    Conflicts:
    	CreateNumber.cpp
    	CreateNumber.h
    	Perceptron.cpp
    	Perceptron.h
    	main.cpp

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -2,8 +2,9 @@
 
 CreateNumber c1;
 #include <iostream>
+
 void Perceptron::setWeights(int choice) {
-    int size = (c1.columns * c1.rows)+1;//Matriz number size + bias
+    int size = (c1.columns * c1.rows) + 1; //Matriz number size + bias
     this->weights = new double[size]();
 
     if (choice == 1) {
@@ -12,27 +13,37 @@ void Perceptron::setWeights(int choice) {
     }
 }
 
-
 double Perceptron::train(CreateNumber number, double *weights) {
-    int i,j,v_weights=0;
-    double sum=0;
-    int erros=0;
-    #pragma omp parallel for reduction(+:sum) 
-    for(i=0;i<number.rows;++i){
-        for(j=0;j<number.columns;++j){
-            sum+=number.getMatrix()[i][j]*weights[v_weights];
-            v_weights++;
+    int i, j, v_weights = 0;
+    double sum = 0;
+    int erros = 0;
+
+    if (weightsAnalyze(number,weights)!= number.getNumber()) {
+        erros++;
+        v_weights = 0;
+        //Weights adjustment
+    #pragma omp parallel for    
+        for (i = 0; i < number.rows; ++i) {
+            for (j = 0; j < number.columns; ++j) {
+                this->weights[v_weights] = this->weights[v_weights]+
+                        ((this->learningRate * (number.getNumber() - sum)) * number.getMatrix()[i][j]);
+                v_weights++;
+            }
         }
     }
-    sum+=number.getBias()*weights[number.columns*number.rows];
-    if(sum>0)
-        sum=1;
-    else
-        sum=0;
-    if(sum != number.getNumber()){
-        erros++;
-        //Weights adjustment
-        
-    }
+}
+
+int Perceptron::weightsAnalyze(CreateNumber number, double *weights) {
+    int i, j, v_weights = 0;
+    double sum = 0;
+    #pragma omp parallel for reduction(+:sum) 
+        for (i = 0; i < number.rows; ++i) {
+            for (j = 0; j < number.columns; ++j) {
+                sum += number.getMatrix()[i][j] * weights[v_weights];
+                v_weights++;
+            }
+        }
+    sum += number.getBias() * weights[number.columns * number.rows];
+    return sum>0 ? 1 : 0;
 }
 
